f61d940b8a6a7c782cdd8eadeda53771e12e5c82
Cyprian Beauvois <cipdu@pcfixe>
2024-12-18 08:55:48 +0100

    devops for deploy

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+import tailwindConfig from './tailwind.config.js'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
+
+export default {
+  plugins: [tailwind(tailwindConfig), autoprefixer],
 }
